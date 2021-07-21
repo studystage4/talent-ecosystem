fa074c4baf95b424aa9fc25a55b02a6e5cc98d32
zns0123 <80972547+zns0123@users.noreply.github.com>
2021-07-22 00:07:38 +0800

    2.1

--- talent-ecosyste-client-8881/src/main/java/com/street/ClientProvider8881.java
+++ talent-ecosyste-client-8881/src/main/java/com/street/ClientProvider8881.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientProvider8881 {
+    // cykj-yyds
     public static void main(String[] args) {
         SpringApplication.run(ClientProvider8881.class,args);
     }
