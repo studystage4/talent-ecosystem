ad9340382517ca5d7f3792d48addef043b772fa2
OldStreet1 <1134917311@qq.com>
2021-07-22 08:53:40 +0800

    Hjj1.0

--- talent-ecosyste-school-8884/src/main/java/com/street/SchoolProvider8884.java
+++ talent-ecosyste-school-8884/src/main/java/com/street/SchoolProvider8884.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class SchoolProvider8884 {
-    //fffff
     public static void main(String[] args) {
         SpringApplication.run(SchoolProvider8884.class,args);
     }
